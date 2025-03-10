8c4ee89ce466f58c6a6503823d00bcb1516a5d9e
Bikash <tulsiojhaamg@gmail.com>
2025-03-10 11:35:36 +0545

    fixed incorrect number of days in baishak 2082

--- src/nepali-date/index.ts
+++ src/nepali-date/index.ts
@@ -1,5 +1,22 @@
 import NepaliDate from './nepali-date';
 
-export { toAD, toBS } from './nepali-date';
+export { toAD, toBS, formatADDate } from './nepali-date';
 
-export default NepaliDate
+export {
+  AD_MONTH,
+  AD_MONTH_NEPALI,
+  ENGLISH_NEPALI_MONTH,
+  ENGLISH_WEEK,
+  NEPALI_MONTH,
+  NEPALI_TODAY,
+  NEPALI_WEEK,
+  AD_MONTH_FULL,
+  ENGLISH_WEEK_FULL,
+  NEPALI_DIGITS,
+  NEPALI_WEEK_FULL,
+  lowerADDate,
+  startDateBS,
+  yearMonthDays,
+} from '../utils/data';
+
+export default NepaliDate;
